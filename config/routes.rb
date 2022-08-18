e114a5b22fa1ce267dccbb55890a9f6aaf836ef3
mdiazc <miguelangel.diaz.kommit@gmail.com>
2022-08-18 17:21:37 -0500

    Added: View and Controller connection to say Hi!

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "articles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/articles", to: "articles#index"
   # Defines the root path route ("/")
