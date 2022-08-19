73657ab271b5bef275f87dee60543abea9c1a02d
mdiazc <miguelangel.diaz.kommit@gmail.com>
2022-08-19 16:21:49 -0500

    Story: RB-21 Generating Controller
    As a User I need to add comments into articles
    
    - Added: comment controller was generated
    
    - Added: add-comment form view inside articles show view
    
    - Added: create method on comments controller
    
    - Added: comment-list view component in articles show view
    
    Story: RB-20 Generating Model
    
    As a user I want my comments to be saved.
    
    - Added: Comment model, migration and test files
    
    - Added: Comment model added to the Schema
    
    - Added: one-to-many relation on Article model
    
    - Added: resourceful routing for comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "articles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
