3f27778463b52fc02d7839f38ba3d3d56ece3730
mdiazc <miguelangel.diaz.kommit@gmail.com>
2022-08-29 10:31:07 -0500

    Story: RB-38 Add devise to the project
    As a user I need a way to log in into the blog
    
    - Added: divise gem to the project
    
    - Added: devise gem installed
    
    - Changed: a default url was added to full fill the recommendations of
    the devise's installer =>
    "1. Ensure you have defined default url options in your environments
    files. Here is an example of default_url_options appropriate for a
    development environment in config/environments/development.rb"
    
    - Changed: notification alerts were added to fullfil the recommendations
    of the devise's installer => 3. Ensure you have flash messages in
    app/views/layouts/application.html.erb.
    
    - Changed: some environment variables were enabled
    
    Story: RB-39 Implement mvc for User
    
    As a user I need a way to log in into the blog
    
    - Added: model and migration for user with devise
    
    - Added: users table by running devise generated migrations
    
    - Added: devise setup for application controller and routes
    
    - Added: devise generated views by running "rails generate devise:views"
    
    - Added: devise generated controllers by running "rails generate devise:controllers users" command
    
    - Added: bootstrap layout for user login and registration
    
    Story: RB-40 Restrict comments for login users
    
    only logged users can comment in other blogs
    
    - Added: route configuration for with devise
    
    - Removed: RoR Basic Authentication no longer needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   root 'articles#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :articles do
