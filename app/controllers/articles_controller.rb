3dd9bcb6904686f54a4c81f5ce09631f4414324b
mdiazc <miguelangel.diaz.kommit@gmail.com>
2022-09-12 13:48:41 -0500

    removed: user setted as the author of an article in create controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -51,7 +51,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = current_user
+    # @article.user = current_user
     if @article.save
       redirect_to @article
     else
